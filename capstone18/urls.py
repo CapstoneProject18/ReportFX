b1914fd91047e651326012d81bbddd8cba958b55
akshat122 <akshat.khanna@st.niituniversity.in>
2018-11-22 23:20:20 +0530

    Updated server files

--- capstone18/urls.py
+++ capstone18/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^capstone/',include('capstone.urls')),
+    # url(r'^capstone/',include('capstone.urls')),
     url(r'^cap/',include('cap.urls'))
 ]

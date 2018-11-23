d466e820ee7bd06966653da3fecb59c904e302f6
akshat122 <akshat.khanna@st.niituniversity.in>
2018-11-23 15:27:10 +0530

    updated the links

--- cap/views.py
+++ cap/views.py
@@ -15,7 +15,9 @@ def index(request):
     return render(request,'web/home.html',{'CPU1': res[0][0],'CPU1_model': res[0][1],'CPU2': res[1][0],'CPU2_model': res[1][1],'CPU3': res[2][0],'CPU3_model': res[2][1]})
 
 def Step2(request):
-    return render(request,'web/Step2.html')
+    res = BI.get_gpu_recommendation()
+    return render(request,'web/Step2.html',{'GPU1': res[0][0],'GPU1_model': res[0][1],'GPU2': res[1][0],'GPU2_model': res[1][1],'GPU3': res[2][0],'GPU3_model': res[2][1]})
 
 def Step3(request):
+    # res = BI.get_memory_recommendation()
     return render(request,'web/Step3.html')
